29fa8b3ebad96d31f602f2579333a456fbd9a326
miquelarranz <miquelarranz@github.com>
2017-10-11 20:14:35 +0200

    fix: Show a not found page if the post does not exist
    It has been added a not found message if someone tries to access
    a post that does not exists anymore.

--- src/containers/PostView.js
+++ src/containers/PostView.js
@@ -46,60 +46,77 @@ class PostView extends Component {
 		const { posts, match } = this.props
 		const { editPostModalOpened, redirectOnDelete } = this.state
 		let post = {}
+		let showNotFound = false;
 
 		let filteredPosts = posts.filter((post) => post.id === match.params.postId)
 		if (filteredPosts.length > 0) post = filteredPosts[0]
+		else showNotFound = true
 
     return (
 			<div>
-				{(redirectOnDelete || post.delete) &&
-					<Redirect to='/'/>
-				}
-
-				<section className="hero is-primary">
-					<div className="hero-body">
-						<div className="container">
-							<h1 className="is-size-3">
-								{post.title}
-							</h1>
+				{showNotFound &&
+					<section className="hero is-primary">
+						<div className="hero-body">
+							<div className="container">
+								<h1 className="is-size-3">
+									404 - Post not found
+								</h1>
+							</div>
 						</div>
+					</section>
+				}
+				{!showNotFound &&
+					<div>
+						{(redirectOnDelete || post.delete) &&
+							<Redirect to='/'/>
+						}
+
+						<section className="hero is-primary">
+							<div className="hero-body">
+								<div className="container">
+									<h1 className="is-size-3">
+										{post.title}
+									</h1>
+								</div>
+							</div>
+						</section>
+
+						<section className="section">
+				    	<div className="container">
+								<article className="media">
+									<div className="media-left">
+										<PostVoter voteScore={post.voteScore} postId={post.id}></PostVoter>
+									</div>
+									<div className="media-content"></div>
+									<div className="media-right">
+										<a className="button is-small is-success" onClick={() => this.openEditPostModal()}>Edit</a>
+		 								<a className="button is-small is-link" onClick={() => this.deletePost()}>Delete</a>
+									</div>
+								</article>
+
+								<p className="post-view-post-content">
+									{post.body}
+								</p>
+
+								<article className="media">
+								  <div className="media-left">
+							      <small className="has-text-grey-light">by {post.author}</small>
+								  </div>
+								  <div className="media-content"></div>
+									<div className="media-right">
+										<small className="has-text-grey-light">{moment(post.timestamp).format('DD/MM/YYYY HH:mm:ss')}</small>
+									</div>
+								</article>
+
+								<hr></hr>
+
+								<CommentsList postId={post.id}></CommentsList>
+
+								<PostModal status={editPostModalOpened} closePostModal={this.closeEditPostModal} data={post} mode="edit"></PostModal>
+							</div>
+						</section>
 					</div>
-				</section>
-
-				<section className="section">
-		    	<div className="container">
-						<article className="media">
-							<div className="media-left">
-								<PostVoter voteScore={post.voteScore} postId={post.id}></PostVoter>
-							</div>
-							<div className="media-content"></div>
-							<div className="media-right">
-								<a className="button is-small is-success" onClick={() => this.openEditPostModal()}>Edit</a>
- 								<a className="button is-small is-link" onClick={() => this.deletePost()}>Delete</a>
-							</div>
-						</article>
-
-						<p className="post-view-post-content">
-							{post.body}
-						</p>
-
-						<article className="media">
-						  <div className="media-left">
-					      <small className="has-text-grey-light">by {post.author}</small>
-						  </div>
-						  <div className="media-content"></div>
-							<div className="media-right">
-								<small className="has-text-grey-light">{moment(post.timestamp).format('DD/MM/YYYY HH:mm:ss')}</small>
-							</div>
-						</article>
-
-						<hr></hr>
-
-						<CommentsList postId={post.id}></CommentsList>
-
-						<PostModal status={editPostModalOpened} closePostModal={this.closeEditPostModal} data={post} mode="edit"></PostModal>
-					</div>
-				</section>
+				}
 			</div>
     );
   }
